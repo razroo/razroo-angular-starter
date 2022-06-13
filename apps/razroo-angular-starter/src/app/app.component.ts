7c0a3087499a69178ae2a67a491b4437dec3b8de
Charlie Greenman <charlie@razroo.com>
2022-06-14 00:50:51 +0300

    initial commit

---
+++ apps/razroo-angular-starter/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'razroo-angular-starter-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'razroo-angular-starter';
+}
