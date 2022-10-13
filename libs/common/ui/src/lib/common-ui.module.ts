07bc4ded9d43ace56c559e20a342cf30abf614e9
Charlie Greenman <charlie@razroo.com>
2022-10-13 19:30:53 +0300

    Add proper routing.

--- libs/common/ui/src/lib/common-ui.module.ts
+++ libs/common/ui/src/lib/common-ui.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from "./layout/layout.component";
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, RouterModule],
   exports: [LayoutComponent],
   declarations: [LayoutComponent]
 })
