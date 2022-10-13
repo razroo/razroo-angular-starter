8e5b7e34ea64b42d0c13b814dd61178c8fa8979a
Charlie Greenman <charlie@razroo.com>
2022-10-13 17:03:30 +0300

    Add layout component.

--- libs/common/ui/src/lib/common-ui.module.ts
+++ libs/common/ui/src/lib/common-ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from "./layout/layout.component";
 
 @NgModule({
   imports: [CommonModule],
+  exports: [LayoutComponent],
+  declarations: [LayoutComponent]
 })
-export class CommonUiModule {}
+export class CommonUiModule { }
---
+++ libs/common/ui/src/lib/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'razroo-angular-starter-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+})
+export class LayoutComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
