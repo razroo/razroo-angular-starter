11cda7c89dfa5c184f76b0df8085bc4fd5726c46
Charlie Greenman <charlie@razroo.com>
2022-10-13 15:05:19 +0300

    Add home component.

--- libs/home/src/lib/home.module.ts
+++ libs/home/src/lib/home.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
   imports: [CommonModule],
+  exports: [HomeComponent],
+  declarations: [HomeComponent]
 })
-export class HomeModule {}
+export class HomeModule { }
---
+++ libs/home/src/lib/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'razroo-angular-starter-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
