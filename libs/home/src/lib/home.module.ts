d555e5565c864e572d730d7033d70d01fc2262f7
Charlie Greenman <charlie@razroo.com>
2022-10-14 13:50:53 +0300

    Add home routing to the application.

--- libs/home/src/lib/home.module.ts
+++ libs/home/src/lib/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from "./home/home.component";
+import { HomeRoutingModule } from "./routing/home-routing.module";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, HomeRoutingModule],
   exports: [HomeComponent],
   declarations: [HomeComponent]
 })
