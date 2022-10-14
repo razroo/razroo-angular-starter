978314cce15e83491d37cc99c19fdbebc019e962
Charlie Greenman <charlie@razroo.com>
2022-10-14 13:31:24 +0300

    Add home routing.

--- libs/home/src/lib/routing/home-routing.module.ts
+++ libs/home/src/lib/routing/home-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from '../home/home.component';
+import { LayoutComponent } from '@razroo-angular-starter/common/ui';
 
 const homeRoutes: Routes = [
   {
