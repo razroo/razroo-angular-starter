7bd770cfbf8e2a4b48b0d646dcf1350106a0675c
Charlie Greenman <charlie@razroo.com>
2022-10-15 10:21:21 +0300

    Add global header.

--- libs/common/ui/src/lib/global-header/global-header.component.spec.ts
+++ libs/common/ui/src/lib/global-header/global-header.component.spec.ts
@@ -1,23 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GlobalHeaderComponent } from './global-header.component';
- 
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
 describe('GlobalComponent', () => {
   let component: GlobalHeaderComponent;
   let fixture: ComponentFixture<GlobalHeaderComponent>;
- 
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GlobalHeaderComponent ]
+      declarations: [GlobalHeaderComponent],
+      imports: [FontAwesomeModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
- 
+
   beforeEach(() => {
     fixture = TestBed.createComponent(GlobalHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
- 
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
