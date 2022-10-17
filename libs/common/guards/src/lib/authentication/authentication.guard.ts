ba9ec467eede7ff6c9716757c0884283cc444eb5
Charlie Greenman <charlie@razroo.com>
2022-10-17 16:07:45 +0300

    Add guard to the application.

---
+++ libs/common/guards/src/lib/authentication/authentication.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  
+  constructor(private router: Router, private oauthService: OAuthService) {}
+
+  canActivate() {
+    if (
+      this.oauthService.hasValidAccessToken() &&
+      this.oauthService.hasValidIdToken()
+    ) {
+      return true;
+    } else {
+      this.router.navigate(['/home', { login: true }]);
+      return false;
+    }
+  }
+}
