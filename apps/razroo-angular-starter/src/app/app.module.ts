7251985879e7023a81650693fa95d25ec53e4425
Charlie Greenman <charlie@razroo.com>
2022-10-17 15:20:27 +0300

    Add footer to the appliction.

--- apps/razroo-angular-starter/src/app/app.module.ts
+++ apps/razroo-angular-starter/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./routing/app-routing.module";
 import { GlobalHeaderModule } from "@razroo-angular-starter/common/ui";
+import { GlobalFooterModule } from "@razroo-angular-starter/common/ui";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderModule, GlobalFooterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
