0a3e25601d8fa805669beac3fb617dbbee73a98a
Charlie Greenman <charlie@razroo.com>
2022-10-17 15:52:49 +0300

    Add ngrx store devtools.

--- apps/razroo-angular-starter/src/app/app.module.ts
+++ apps/razroo-angular-starter/src/app/app.module.ts
@@ -7,10 +7,17 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./routing/app-routing.module";
 import { GlobalHeaderModule } from "@razroo-angular-starter/common/ui";
 import { GlobalFooterModule } from "@razroo-angular-starter/common/ui";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { StoreModule } from "@ngrx/store";
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderModule, GlobalFooterModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderModule, GlobalFooterModule, StoreModule.forRoot({}), StoreDevtoolsModule.instrument({
+    maxAge: 25, // Retains last 25 states
+    logOnly: environment.production, // Restrict extension to log-only mode
+    autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+  }),],
   providers: [],
   bootstrap: [AppComponent],
 })
