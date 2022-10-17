3c6b2f27f51116b01110a7d8343d95e400aceadb
Charlie Greenman <charlie@razroo.com>
2022-10-17 15:57:48 +0300

    Add common services.

---
+++ libs/common/services/README.md
@@ -0,0 +1,7 @@
+# common-services
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test common-services` to execute the unit tests.
---
+++ libs/common/services/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'common-services',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/common/services',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
