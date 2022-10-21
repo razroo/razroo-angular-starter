765b35c0cda82ef42276c9d54f783fe3487667bd
Charlie Greenman <charlie@razroo.com>
2022-10-21 09:45:37 +0300

    Add service for razroo application.

--- libs/data-services/src/index.ts
+++ libs/data-services/src/index.ts
@@ -1,3 +1,2 @@
 export * from './lib/data-services.module';
 export * from "./lib/user/user.service";
-export * from "./lib/user/user.service";
--- libs/data-services/src/lib/user/user.service.ts
+++ libs/data-services/src/lib/user/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { from } from 'rxjs';
+import { pluck } from 'rxjs/operators';
+import { UserQuery } from "@razroo-angular-starter/data-graphql";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
+
+  user() {
+    const query = UserQuery;
+    const variables = {};
+    const user$ = this.apollo.query({ query, variables });
+    return from(user$).pipe(pluck('data', 'user'));
+  }
 }
