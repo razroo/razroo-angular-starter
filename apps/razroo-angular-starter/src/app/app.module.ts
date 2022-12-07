a30dd80deafac0aa9b8e334e12c7d9ac53b26de1
Charlie Greenman <charlie@razroo.com>
2022-12-07 10:58:21 +0200

    Add material and add button.

--- apps/razroo-angular-starter/src/app/app.module.ts
+++ apps/razroo-angular-starter/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,8 +6,11 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+  ],
+  imports: [BrowserModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
